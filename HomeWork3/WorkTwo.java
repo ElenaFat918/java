76110e996802f521946497cd7e1bc6964f59d78d
Elena <keaya@rambler.ru>
2022-12-13 15:05:23 +0300

    задача2. Пусть дан произвольный список целых чисел, удалить из него четные числа.

--- HomeWork3/WorkTwo.java
+++ HomeWork3/WorkTwo.java
@@ -1,12 +1,12 @@
 package HomeWork3;
+
 import java.util.ArrayList;
 import java.util.Iterator;
 
-
 public class WorkTwo {
     ArrayList<Integer> list = new ArrayList<Integer>();
-    
-    public void addAndRemoveNumbers(){
+
+    public void addAndRemoveNumbers() {
         list.add(56789); // на i-й индекс добавляет элемент
         list.add(3);
         list.add(34);
@@ -15,18 +15,17 @@ public class WorkTwo {
         list.add(33);
         list.add(55);
         list.add(0);
-       
+
         System.out.println("Первичный список целых чисел" + list);
         Iterator<Integer> iterator = list.iterator();
         while (iterator.hasNext()) {
-            if (iterator.next()%2==0) { 
+            if (iterator.next() % 2 == 0) {
                 iterator.remove();
             }
         }
         System.out.println("Cписок оставшихся нечетных чисел" + list);
-       
     }
 }
 /*
- * Пусть дан произвольный список целых чисел, удалить из него четные числа
+ * Пусть дан произвольный список целых чисел, удалить из него четные числа.
  */

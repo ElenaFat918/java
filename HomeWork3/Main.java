86dd78e5af4d94687cdfd9740ea97d7f13b4d4cb
Elena <keaya@rambler.ru>
2022-12-13 15:03:48 +0300

    Main 3й домашней работы

--- HomeWork3/Main.java
+++ HomeWork3/Main.java
@@ -2,14 +2,13 @@ package HomeWork3;
 
 public class Main {
     public static void main(String[] args) {
-        WorkOne workOne = new WorkOne();
+        WorkOne workOne = new WorkOne(); 
         workOne.taskOne();
 
+        WorkTwo workTwo = new WorkTwo();
+        workTwo.addAndRemoveNumbers();
 
-        // WorkTwo workTwo = new WorkTwo();
-        // workTwo.addAndRemoveNumbers();
-
-        // WorkThree workThree = new WorkThree();
-        // workThree.findMinMaxAverage();
+        WorkThree workThree = new WorkThree();
+        workThree.findMinMaxAverage();
     }
 }

5fb61b3b8adf4dc19a64df6e405285210126b68d
Elena <keaya@rambler.ru>
2022-12-26 21:11:58 +0300

    Домашняя работа5 map? hashmap..

---
+++ HomeWork5/Main.java
@@ -0,0 +1,25 @@
+package HomeWork5;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+public class Main{
+    public static void main(String[] args) {
+        // PhonebookHashMap phonebookHashMap = new PhonebookHashMap();
+        // phonebookHashMap.fillHashMap();
+        // phonebookHashMap.getcontactsInfoByName();
+        
+     
+        // ListToMap listToMap = new ListToMap();
+        // System.out.println();
+        // ArrayList<String> staff = listToMap.addStaff(); 
+        // System.out.println();
+        // Map<String, Integer> name = listToMap.repeat(staff);
+        // System.out.println();
+        // listToMap.sorted(name);
+        // listToMap.run();
+
+        RomanNumConv romanNumConv = new RomanNumConv();
+        romanNumConv.solution();
+    }
+}
